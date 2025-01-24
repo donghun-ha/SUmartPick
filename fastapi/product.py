8fe56bb77a69e686ad906652b82e00351a1dd9c5
하동훈 <harukax99@naver.com>
2025-01-24 14:08:33 +0900

    fix: table categories => category

--- fastapi/product.py
+++ fastapi/product.py
@@ -176,7 +176,7 @@ async def select():
     결과값을 딕셔너리로 변환할때 쓰이는 SQL문장
     지금 적혀있는건 더미값을 제외한 값을 출력하는 문장이다. 더미값을 지우면 >=430을 지워야 함
     """
-    sql = "select P.preview_image, P.Product_ID, P.name, C.name, P.created, P.price from products as P, categories as C where C.Category_ID = P.Category_ID and P.Product_ID >= 430"
+    sql = "select P.preview_image, P.Product_ID, P.name, C.name, P.created, P.price from products as P, category as C where C.Category_ID = P.Category_ID and P.Product_ID >= 430"
     curs.execute(sql)
     rows = curs.fetchall()
     conn.close()
