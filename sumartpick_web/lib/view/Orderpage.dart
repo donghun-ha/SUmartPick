96be625f3f44b1160a1113655e7151a1e1b27a88
MoJJiGamJya <bowwow69@naver.com>
2025-02-05 15:05:57 +0900

    order_select fastAPI수정

--- sumartpick_web/lib/view/Orderpage.dart
+++ sumartpick_web/lib/view/Orderpage.dart
@@ -36,7 +36,7 @@ class _OrderpageState extends State<Orderpage> {
     super.initState();
     data = [];
     filteredOrders = [];
-    keys = ["주문번호", "주문상세번호", "주문ID", "상품명", "상품금액", "주문일시", "배송주소", "환불요청시간", "환불시간", "결제방법", "배송도착시간", "배송상태"];
+    keys = ["주문번호", "주문상세번호", "주문ID", "상품명", "상품금액", "주문일시", "배송주소", "환불요청시간", "환불시간", "결제방법", "배송도착시간", "배송상태", "상품ID"];
     selectedFilter = "주문번호";
     searchController = TextEditingController();
     startDate = null; // 초기값 null
@@ -572,7 +572,8 @@ class _OrderpageState extends State<Orderpage> {
                                           product['주문상세번호'],
                                           product['환불요청시간'],
                                           product['배송도착시간'],
-                                          product['배송상태']
+                                          product['배송상태'],
+                                          product['상품ID']
                                           ]
                                           )!.then((value) => getJSONData(),);
                                       },
